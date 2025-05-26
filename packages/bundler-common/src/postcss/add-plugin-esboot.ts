474510c0a8f9d9d1787b4b573c5ea384e5b45b08
Roc <moonlitusun@gmail.com>
2025-05-26 18:01:18 +0800

    feat(postcss-plugin-esboot): change to esm

--- packages/bundler-common/src/postcss/add-plugin-esboot.ts
+++ packages/bundler-common/src/postcss/add-plugin-esboot.ts
@@ -3,7 +3,6 @@ import type { ConfigurationInstance } from '@dz-web/esboot';
 export const addPostcssPluginESBoot = async (cfg: ConfigurationInstance) => {
   const { useTailwindcss } = cfg.config;
 
-  // @ts-ignore
   return import('@dz-web/postcss-plugin-esboot').then(({ default: plugin }) =>
     plugin({
       useTailwindcss,
