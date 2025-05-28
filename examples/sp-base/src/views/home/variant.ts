917d10b569e5fb6128af77bee25b157c75944f65
Roc <moonlitusun@gmail.com>
2025-05-28 18:15:15 +0800

    feat(plugin-docs): support esm

--- examples/sp-base/src/views/home/variant.ts
+++ examples/sp-base/src/views/home/variant.ts
@@ -1,7 +1,5 @@
 import { cva } from '@dz-web/esboot-browser';
 
-console.log(cva, 'cva');
-
 export const langBtn = cva(
   ' flex h-[32px] w-[36px] items-center justify-center rounded-[4px] text-[14px] text-[#999] pointer ',
   {
