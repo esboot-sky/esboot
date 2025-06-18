759a777cbcd74917347a9aa1ad59bcbe9c5111dc
Roc <moonlitusun@gmail.com>
2025-06-18 23:14:26 +0800

    feat(eslint): upgrade eslint

--- packages/esboot/src/cli/prepare/index.ts
+++ packages/esboot/src/cli/prepare/index.ts
@@ -7,7 +7,7 @@ import {
 } from '@/plugin';
 import { generateTypeScriptCfg } from './generate-typescript-cfg';
 import { generateStylelintCfg } from './generate-stylelint-cfg';
-import { generateESLintCfg } from './generate-eslint-cfg';
+// import { generateESLintCfg } from './generate-eslint-cfg';
 import { generatePrettierCfg } from './generate-prettier-cfg';
 import { generateCommitlintCfg } from './generate-commitlint-cfg';
 import { generateTypeScriptTypes } from './generate-typescript-types';
@@ -23,7 +23,7 @@ export function prepare() {
   if (!isCIBuild) {
     generateStylelintCfg();
     generatePrettierCfg();
-    generateESLintCfg();
+    // generateESLintCfg();
     generateCommitlintCfg();
     updateVSCodeSetting();
 
