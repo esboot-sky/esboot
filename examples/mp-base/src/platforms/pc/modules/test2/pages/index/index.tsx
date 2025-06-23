36717f1d52939501856430839839c75c235cc377
Roc <moonlitusun@gmail.com>
2025-06-23 23:21:04 +0800

    feat(eslint): add eslint-plugin-better-tailwindcss

--- examples/mp-base/src/platforms/pc/modules/test2/pages/index/index.tsx
+++ examples/mp-base/src/platforms/pc/modules/test2/pages/index/index.tsx
@@ -2,11 +2,10 @@ import { Link } from 'react-router-dom';
 
 import CheckIcon from '@/images/check.svg';
 import CheckIcon2 from '@/images/check.svg?url';
-import './index.scss';
 import { genericMemo } from '@/utils/react-utils';
+import './index.scss';
 
-
-const Index = genericMemo(function Index() {
+const Index = genericMemo(() => {
   return (
     <div>
       <Link
@@ -19,9 +18,10 @@ const Index = genericMemo(function Index() {
       </Link>
 
       <CheckIcon width={100} height={100} />
-      <img src={CheckIcon2} alt="" className="w-10 h-10" />
+      <img src={CheckIcon2} alt="" className="h-10 w-10" />
       <img src="/static/logo.png" alt="" />
-      <div styleName="w375" className="text-bold text-red-500 p-5">
+
+      <div styleName="w375" className="text-bold m-5 p-5 text-red-500">
         375 width in 750 design
       </div>
     </div>
