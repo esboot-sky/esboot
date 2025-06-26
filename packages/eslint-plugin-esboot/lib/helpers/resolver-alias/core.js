62dbd8884e887d33ae4e5a523b54a0f9d30193eb
Roc <moonlitusun@gmail.com>
2025-06-26 23:29:02 +0800

    feat(bundler-webpack): support react compiler

--- packages/eslint-plugin-esboot/lib/helpers/resolver-alias/core.js
+++ packages/eslint-plugin-esboot/lib/helpers/resolver-alias/core.js
@@ -1,10 +1,1 @@
-import { builtinModules } from 'node:module';
-
-const coreModules = Object.create(null);
-const modules = builtinModules || /* istanbul ignore next */ Object.keys(process.binding('natives')).filter(m => m.indexOf('internal') !== 0);
-
-modules.forEach(function (m) {
-  coreModules[m] = true;
-});
-
-export default coreModules;
+module.exports = module.constructor.builtinModules || /* istanbul ignore next */ Object.keys(process.binding('natives')).filter(m => m.indexOf('internal') !== 0);
