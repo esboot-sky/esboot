1fac9276e495ed36a9f9fd242f191885b07b2d23
Roc <moonlitusun@gmail.com>
2025-08-23 23:27:41 +0800

    fix(postcss-plugin-esboot): build error

--- packages/postcss-plugin-esboot/tsup.config.ts
+++ packages/postcss-plugin-esboot/tsup.config.ts
@@ -2,10 +2,6 @@ import { defineConfig } from '../../tsup.base';
 
 export default defineConfig({
   base: {
-    // Prevent dynamic require issues with Node.js built-in modules
-    noExternal: ['*'],
     format: ['esm'],
-    // Tell tsup to bundle external Node.js modules
-    external: []
   }
 });
