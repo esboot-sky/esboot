d0b91ee2308af405cf2b2bc7dca7257de2bb52fd
Roc <moonlitusun@gmail.com>
2025-08-24 12:57:48 +0800

    feat: replace glob to tinyglobby

--- examples/sp-base/.esbootrc.ts
+++ examples/sp-base/.esbootrc.ts
@@ -1,10 +1,6 @@
-import type { Configuration } from '@dz-web/esboot';
-import type { BundlerWebpackOptions as BundlerOptions } from '@dz-web/esboot-bundler-webpack';
+import type { BundlerViteOptions as BundlerOptions } from '@dz-web/esboot-bundler-vite';
 import { defineConfig, definePlugin, PluginHooks } from '@dz-web/esboot';
-import {
-  BundlerWebpack as Bundler,
-  CodeSplittingType,
-} from '@dz-web/esboot-bundler-webpack';
+import { BundlerVite as Bundler } from '@dz-web/esboot-bundler-vite';
 import pluginDocs from '@dz-web/esboot-plugin-docs';
 import pluginVitest from '@dz-web/esboot-plugin-vitest';
 
@@ -14,86 +10,20 @@ export default defineConfig<BundlerOptions>({
     pluginVitest(),
     definePlugin({
       key: 'test1',
-      // onActivated: () => {
-      //   console.log('test plugin onActivated');
-      // },
-      // [PluginHooks.modifyConfig]: (config) => {
-      //   // console.log('modifyBundlerConfig', config);
-      //   return {
-      //     // publicPath: '/tetett/',
-      //   };
-      // },
-      // [PluginHooks.registerCommands]: (cfg) => {
-      //   return [
-      //     {
-      //       name: 'test1',
-      //       description: 'testlkjsjdfklsjdlkf',
-      //       options: ['-f, --file <char>', '-s, --sampleFile <char>'],
-      //       action: (options) => {
-      //         console.log('tes234324234t', options);
-      //       },
-      //     },
-      //   ];
-      // },
-      // [PluginHooks.modifyTypescriptConfig]: (cfg, result) => {
-      //   return {
-      //     compilerOptions: {
-      //       baseUrl: 'src',
-      //     },
-      //   };
-      // },
-      // [PluginHooks.modifyPrettierConfig]: (cfg, result) => {
-      //   return {
-      //     printWidth: 1000,
-      //   };
-      // },
-      // [PluginHooks.modifyStylelintConfig]: (cfg, result) => {
-      //   return {
-      //     printWidth: 1000,
-      //   };
-      // },
-      // [PluginHooks.modifyEslintConfig]: (cfg, result) => {
-      //   return {
-      //     printWidth: 1000,
-      //   };
-      // },
-      // [PluginHooks.modifyBundlerConfig]: (cfg, bundlerConfig, bundlerName) => {
-      //   console.log(bundlerName, 'name');
-      //   bundlerConfig.output.publicPath = '/modu';
-      // },
       [PluginHooks.afterCompile]: (cfg) => {
         console.log(cfg.entry);
       },
     }),
-    // definePlugin({
-    //   key: 'test2',
-    //   registerCommands: () => {
-    //     return [
-    //       {
-    //         name: 'test2',
-    //         description: 'test2',
-    //         action: () => {
-    //           console.log('test2');
-    //         },
-    //       },
-    //     ];
-    //   },
-    // }),
   ],
   bundler: Bundler,
   isSP: true,
-  bundlerOptions: {
-    mfsu: false,
-    codeSplitting: {
-      jsStrategy: CodeSplittingType.granularChunks,
-    },
-  },
+  bundlerOptions: {},
   sourceMap: false,
   alias: {
     '@@': 'src',
   },
   server: {
-    port: 4200,
+    port: 4000,
     http2: false,
   },
   // analyze: true,
