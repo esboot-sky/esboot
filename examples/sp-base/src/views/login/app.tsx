322f7e17c497995f4f07fcf71b90658a3be5f551
Roc <moonlitusun@gmail.com>
2025-10-26 00:07:11 +0800

    chore:  restrict key in lang json

--- examples/sp-base/src/views/login/app.tsx
+++ examples/sp-base/src/views/login/app.tsx
@@ -1,6 +1,7 @@
 import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const c = 'bg-red-500 mt-3 bg-red-500 bg-red-500 w-[5px] bg-red-500 bg-red-500 424234234234234 23423423423 234234234 234234234 234234';
 function Login() {
   const navigate = useNavigate();
 
@@ -12,8 +13,8 @@ function Login() {
 
   return (
     <div>
-      <button onClick={clickHandler} type="button" className="pt-5 bg-red-500 mt-3 bg-red-500 bg-red-500 w-[5px]">
-        Toggle sadfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      <button onClick={clickHandler} type="button" className="pt-5 bg-red-500 mt-3 bg-red-500 bg-red-500 w-[5px] bg-red-500 bg-red-500">
+        Toggle sadfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaadfasfasdfsd
       </button>
       登录
       <p>{status ? 'open' : 'close'}</p>
