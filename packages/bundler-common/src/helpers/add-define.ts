4f8f6f85e183953ebec7c6d6956c1204d083eae7
Roc <moonlitusun@gmail.com>
2025-11-18 16:29:44 +0800

    chore: fix lint error

--- packages/bundler-common/src/helpers/add-define.ts
+++ packages/bundler-common/src/helpers/add-define.ts
@@ -1,9 +1,8 @@
 import type { ConfigurationInstance } from '@dz-web/esboot';
-import process from 'node:process';
 
 type AddDefineRes = Record<string, unknown>;
 export function addDefine(cfg: ConfigurationInstance): AddDefineRes {
-  const { version, define = {} } = cfg.config;
+  const { define = {} } = cfg.config;
 
   const customDefine: AddDefineRes = {};
   for (const key in define) {
