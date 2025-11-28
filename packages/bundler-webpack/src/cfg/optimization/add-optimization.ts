bbd43184f2df80e5fc349e3283117337a2e3b619
Roc <moonlitusun@gmail.com>
2025-11-28 11:11:12 +0800

    chore(bundler-webpack): fixing the moduleID is not number

--- packages/bundler-webpack/src/cfg/optimization/add-optimization.ts
+++ packages/bundler-webpack/src/cfg/optimization/add-optimization.ts
@@ -1,24 +1,28 @@
-import { addJSMinimizer } from './add-js-minimizer';
-import { addCSSMinimizer } from './add-css-minimizer';
-import { addCodeSplitting } from './code-splitting/add-code-splitting';
-
 import type { AddFunc } from '@/cfg/types';
+import { addCSSMinimizer } from './add-css-minimizer';
+import { addJSMinimizer } from './add-js-minimizer';
+
+import { addCodeSplitting } from './code-splitting/add-code-splitting';
 
 export const addOptimization: AddFunc = async (cfg, webpackCfg) => {
   const { isDev, minimize } = cfg.config;
 
-  if (isDev) return;
+  if (isDev)
+    return;
 
   webpackCfg.optimization = {
     // splitChunks,
     emitOnErrors: true,
     usedExports: true,
     sideEffects: true,
+    // For fixing the moduleID is not number
+    moduleIds: 'natural',
     minimize,
     minimizer: [],
   };
 
-  if (!minimize) return;
+  if (!minimize)
+    return;
   await addJSMinimizer(cfg, webpackCfg);
   await addCSSMinimizer(cfg, webpackCfg);
   await addCodeSplitting(cfg, webpackCfg);
