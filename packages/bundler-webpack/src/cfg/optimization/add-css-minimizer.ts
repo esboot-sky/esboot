02e0b7f5412e04a7b40fc70750cd5e9a2b37aaef
Roc <moonlitusun@gmail.com>
2025-11-28 11:11:12 +0800

    chore(bundler-webpack): fixing the moduleID is not number

--- packages/bundler-webpack/src/cfg/optimization/add-css-minimizer.ts
+++ packages/bundler-webpack/src/cfg/optimization/add-css-minimizer.ts
@@ -1,12 +1,13 @@
-import { CSSMinifier } from '@dz-web/esboot-common/constants';
-import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
-
 import type { AddFunc } from '@/cfg/types';
+import { CSSMinifier } from '@dz-web/esboot-common/constants';
+
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 
 export const addCSSMinimizer: AddFunc = async (cfg, webpackCfg) => {
   const { cssMinifier = CSSMinifier.cssnano, cssMinifierOptions = {} } = cfg.config;
 
-  if (cssMinifier === CSSMinifier.none) return;
+  if (cssMinifier === CSSMinifier.none)
+    return;
 
   let minifier: CssMinimizerPlugin;
 
