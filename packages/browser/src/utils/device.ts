5efb5e03e747954d4d4066ccf9c14db590a6fc41
Roc <moonlitusun@gmail.com>
2025-12-15 17:51:00 +0800

    chore: rm isDZApp

--- packages/browser/src/utils/device.ts
+++ packages/browser/src/utils/device.ts
@@ -2,5 +2,3 @@ export const userAgent = navigator.userAgent;
 
 export const isIOS = /iphone|ipad/i.test(userAgent);
 export const isAndroid = /android/i.test(userAgent);
-export const isDZApp = /dzapp/i.test(userAgent);
-export const isDZAppByDS = /dzapp-ds/i.test(userAgent);
