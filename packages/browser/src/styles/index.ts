b577cd88de040cb49f4d41581491a060a705b656
Roc <moonlitusun@gmail.com>
2025-12-18 22:35:54 +0800

    chore: release 4.0.5

--- packages/browser/src/styles/index.ts
+++ packages/browser/src/styles/index.ts
@@ -1,5 +1,5 @@
-export { type ClassValue, clsx } from 'clsx';
-export * from 'tailwind-merge';
-export { cva, type VariantProps } from 'class-variance-authority';
-
 export * from './cn';
+export { cva, type VariantProps } from 'class-variance-authority';
+export { type ClassValue, clsx } from 'clsx';
+
+export * from 'tailwind-merge';
