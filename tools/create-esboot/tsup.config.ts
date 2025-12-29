a7caf0e633cadc3b111f402012bd28b794cc15d3
Roc <moonlitusun@gmail.com>
2025-12-29 15:05:50 +0800

    fix(create-esboot): missing cli.js  gitlab(#1)

--- tools/create-esboot/tsup.config.ts
+++ tools/create-esboot/tsup.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from '../../tsup.base';
 export default defineConfig({
   base: {
     dts: false,
+    legacyOutput: true,
+    format: ['esm', 'cjs'],
     entry: ['src/cli.ts'],
   },
 });
